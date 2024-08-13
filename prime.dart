f621f74b2c83ae1b8587f147099ba2b53dbd1720
badusha6555 <badusha524454@gmail.com>
2024-08-13 12:08:35 +0530

    added file

---
+++ prime.dart
@@ -0,0 +1,21 @@
+bool isPrime(int number) {
+  int number = 25;
+  if (number <= 1) {
+    return false;
+  }
+  for (int i = 2; i <= number ~/ i; i++) {
+    if (number % i == 0) {
+      return false;
+    } else {}
+  }
+  return true;
+}
+
+void main() {
+  int number = 93;
+  if (isPrime(number)) {
+    print("The number $number is a prime number.");
+  } else {
+    print("The number $number is not a prime number.");
+  }
+}
